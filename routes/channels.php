65dd860f345860c3ef2c18622a5d688d49bd3dde
Luqman Project <luqmanulhakiem7@gmail.com>
2023-11-14 14:46:35 +0700

    add broadcast auth

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\ChatParticipant;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('chat.{id}', function ($user, $id) {
+    $participant = ChatParticipant::where([
+        [
+            'user_id',$user->id,
+        ],
+        [
+            'chat_id',$id
+        ],
+
+    ])->first();
+    return $participant == null;
 });
